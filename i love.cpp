f8b59cd6bff32f23161ee171d28c29c8be81eb42
Munna kumar <115360737+munnakumar0@users.noreply.github.com>
2022-11-09 10:45:48 +0530

    Add files via upload

---
+++ i love.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int i;
+	
+	for(i=1;i<=1;i++)
+	{
+		printf("I Love you\n");
+		printf("I\n");
+		printf("......Love.......\n");
+		printf("............my........\n");
+		printf(".................India......\n");
+}
+}
