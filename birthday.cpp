3e8b6242fa4c8172b60aa8b16f74b910ffa8b312
Munna kumar <115360737+munnakumar0@users.noreply.github.com>
2022-11-09 11:23:51 +0530

    Add files via upload

---
+++ birthday.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	
+	printf("\n    ......................");
+	printf("\n    :********************:");
+	printf("\n    :***Happy ***********:");
+	printf("\n    :********************:");
+	printf("\n    :***** BIRTHDAY******:");
+	printf("\n    :....................:");
+	printf("\n    :********TO**********:");
+	printf("\n    :********************:");
+	printf("\n    :***********YOU******:");
+	printf("\n    :....................:");
+	printf("\n    :******SHIVAM********:");
+    printf("\n    :********************:");
+	printf("\n    :**********BHAIYA****:");
+	printf("\n    :....................:");
+	printf("\n    :**WISHES BY MUNNA***:");
+	printf("\n    :********************:");
+	printf("\n");
+	printf("\n");
+	getch();
+	return 0;  
+}
