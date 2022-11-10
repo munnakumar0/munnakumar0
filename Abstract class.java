1d771fb6e970059b385238c20fb7ec02a499ca01
Munna kumar <115360737+munnakumar0@users.noreply.github.com>
2022-11-10 10:14:38 +0530

    Add files via upload

---
+++ Abstract class.java
@@ -0,0 +1,16 @@
+Abstract class Bike
+{
+abstract void run();
+}
+class Honda extends Bike
+{
+void run()
+{
+System.out.println("Bike running safely");
+}
+public static void main(String args[])
+{
+Bike b=new Bike();
+b.run();
+}
+}
